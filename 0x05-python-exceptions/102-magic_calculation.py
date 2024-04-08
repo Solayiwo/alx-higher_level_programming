7b78ff5cf60493c3ba61b583d8f5e7695c2acd27
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-04-07 20:50:13 -0700

    ByteCode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -10,7 +10,7 @@ def magic_calculation(a, b):
             else:
                 result += (a ** b) / i
         except Exception:
-            result += a + b
+            result = a + b
             break
 
     return result
