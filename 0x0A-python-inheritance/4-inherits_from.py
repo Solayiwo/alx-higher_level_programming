ece40ac1eb3b999926aaf80ec77b6e50d250050e
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-04-17 06:53:14 -0700

    Code updated

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -17,12 +17,5 @@ def inherits_from(obj, a_class):
         True if obj is an instance of a class that inherited from a_class,
         False otherwise.
     """
-
-    """Get the classes that obj is an instance of"""
-    object_classes = type(obj).__mro__
-
-    """Check if any of these classes are equal to or derived from a_class"""
-    for cls in object_classes:
-        if cls is a_class or issubclass(cls, a_class):
-            return True
-    return False
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
