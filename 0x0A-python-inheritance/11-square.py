d5dc3465ea2eae39326262da659610c493941aef
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-04-17 16:48:26 -0700

    Square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -14,3 +14,9 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def __str__(self):
+        """
+        Function that returns the square description
+        """
+        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
