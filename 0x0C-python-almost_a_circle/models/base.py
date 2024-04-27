ec5d41c843c7524a4774b730f414510138b358a0
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-04-26 20:17:50 -0700

    Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-Defines a base model class.
-"""
+"""Defines a base model class."""
+import json
 
 
 class Base:
@@ -23,3 +22,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON serialization of a list of dicts.
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
