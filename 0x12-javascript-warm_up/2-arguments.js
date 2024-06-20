2c2c29847534d42d6bb76a1adc55e88880c52604
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-06-19 19:05:59 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-const argvCount = process.argv.length;
+const { argv } = require('process');
+const argvCount = argv.length;
+
 if (argvCount === 2) {
   console.log('No argument');
 } else if (argvCount === 3) {
