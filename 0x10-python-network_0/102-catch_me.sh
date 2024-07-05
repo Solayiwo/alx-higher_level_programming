5172ef6b4150d94b7ce29ce64743174f207cd617
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-07-04 20:31:00 -0700

    Catch me if you can!

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# A bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to  message respond 'You got me!', in the response body
+curl -sL -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
