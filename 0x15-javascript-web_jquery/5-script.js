af758b4ea08d1b77ef748531a109ae61f3700900
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-08-09 10:07:47 -0700

    List of elements

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,7 @@
+// JavaScript script that Uses jQuery API to adds a <li> element to a
+// list when the user clicks on the tag DIV#add_item
+
+$('div#add_item').click(function () {
+  const element = '<li>Item</li>';
+  $('ul.my_list').append(element);
+});
