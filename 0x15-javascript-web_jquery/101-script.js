05c1de5bda81eec54883e274dd6b60730813161f
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-08-09 13:33:37 -0700

    List, add, remove

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,16 @@
+// JavaScript script that adds, removes and clears LI elements from a list when the user clicks
+
+$(document).ready(function () {
+  $('#add_item').click(function () {
+    const newItem = $('<li>Item</li>');
+    $('ul.my_list').append(newItem);
+  });
+
+  $('#remove_item').click(function () {
+    $('ul.my_list li').last().remove();
+  });
+
+  $('#clear_list').click(function () {
+    $('ul.my_list').empty();
+  });
+});
